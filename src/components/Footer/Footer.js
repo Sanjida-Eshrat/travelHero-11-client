a03744b4fd53b1a1fa00f5b57c81c759ff9cd4ed
Sanjida-Eshrat <sanjida.kh023@gmail.com>
2021-11-24 01:39:50 +0600

    footer changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,61 +2,106 @@ import React from 'react';
 import './Footer.css';
 const Footer = () => {
     return (
-        <div>
-           <div className="footer-container">
-                <div className="container">
-                    <div className="row d-flex">
-                    <div className="col-md-5 col-12">
-                        <div className="left-container text-start">
-                        <h1 className="text-anim"><span style={{color:'orange'}}>Travel</span><span style={{color:'#5356ad'}}>Hero</span></h1>
-                        <p className="mt-4 ">
+    <div style={{backgroundColor:' #aeb0df'}}>
+        {/* <!-- Footer  */}
+        <footer
+                className="text-center text-lg-start text-white"
+                >
+            {/* Grid container  */}
+            <div className=" p-4 pb-0">
+            {/* Section: Links  */}
+            <section className="">
+                {/*Grid row */}
+                <div className="row">
+                {/* Grid column  */}
+                    <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h1 className="text-anim"><span style={{color:'orange'}}>Travel</span><span style={{color:'#5356ad'}}>Hero</span></h1>
+                        <p>
                             <small>
-                              Make memories with TravelHero.
+                                Make memories with TravelHero.
                             </small>
                         </p>
-        
-                        <p className="mt-5">
-                            <small>travelHero © . All rights reserved.</small>
-                        </p>
-                        </div>
                     </div>
-                    <div className="col-md-2  col-12">
-                        <div className="footer-menu-container">
-                        <ul>
-                            <li className="footer-menu">Home</li>
-                            <li className="footer-menu">About</li>
-                            <li className="footer-menu">Tours</li>
-                            <li className="footer-menu">Services</li>
-                        </ul>
-                        </div>
-                    </div>
-                    <div className="col-md-5">
-                        <div className="right-footer-container">
-                        <h3>For any enquiry</h3>
-                        <input
-                            className="footer-input"
-                            type="text"
-                            placeholder="Enter Email"
-                        />
-                        <div className="phone d-flex align-items-center justify-content-center mt-4">
+                    {/* Grid column  */}
 
-                            <div>
-                            <h5>+1 8 800 999 35 35</h5>
-                            </div>
-                        </div>
-                        <div className="map d-flex align-items-center justify-content-center">
-                            <div>
-                            <p>
-                                102 1st Avenue, New York, NY 100
-                            </p>
-                            </div>
-                        </div>
-                        </div>
+                    <hr className="w-100  d-md-none" />
+
+                    {/* Grid column  */}
+                    <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <p className="text-white">
+                            Home
+                        </p>
+                        <p className="text-white">
+                            About
+                        </p>
+                        <p className="text-white">
+                        Tour
+                        </p>
+                        <p className="text-white">
+                        Services
+                        </p>
                     </div>
+                    {/* Grid column  */}
+
+                    <hr className="w-100 d-md-none" />
+
+                    {/* Grid column  */}
+                    <hr className="w-100 d-md-none" />
+
+                    {/* Grid column  */}
+                    <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h5 className="mb-4 font-weight-bold">Contact</h5>
+                        <p><i className="fas fa-home mr-3"></i> Rampura, Dhaka, Bangladesh</p>
+                        <p><i className="fas fa-envelope mr-3"></i> travelhero@gmail.com</p>
+                        <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
                     </div>
-                </div>
+                    {/* Grid column  */}
+
+                    {/* Grid column  */}
+                    <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h5 className="mb-4 font-weight-bold">Follow us</h5>
+
+                        {/* Facebook  */}
+                        <a
+                        className="btn m-1 text-white"
+                        href='#!' style={{backgroundColor: '#3b5998'}}
+                        ><i className="fab fa-facebook-f"></i
+                        ></a>
+
+                        {/* Twitter  */}
+                        <a
+                        className="btn m-1 text-white"
+                        href='#!' style={{backgroundColor:'#55acee'}}
+                        ><i className="fab fa-twitter"></i></a>
+
+                        {/* Google  */}
+                        <a
+                        className="btn m-1 text-white"
+                        href='#!' style={{backgroundColor:'#dd4b39'}}
+                        ><i className="fab fa-google"></i
+                        ></a>
+
+                        {/* Instagram  */}
+                        <a
+                        className="btn m-1 text-white"
+                           href='#!' style={{backgroundColor:'#ac2bac'}}
+                        ><i className="fab fa-instagram"></i></a>
+                    </div>
+               </div>
+                {/*Grid row */}
+            </section>
+            {/* Section: Links  */}
             </div>
+            {/* Grid container  */}
+        </footer>
+        {/* Footer  */}
+        {/* Copyright  */}
+        <div className=" text-center p-2 text-white" style={{backgroundColor: '#5356ad'}}>
+            © 2021 Copyright: TravelHero         
         </div>
+        {/*  Copyright  */}
+        
+    </div>
     );
 };
 
